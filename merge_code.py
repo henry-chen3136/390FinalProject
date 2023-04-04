ef7c326bff567ec447d07cc2654b4f112b028e93
cassieg0 <c.greidanus@queensu.ca>
2023-04-04 19:28:44 -0400

    overall DS created

--- merge_code.py
+++ merge_code.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-merge1 = ['C:/Users/Cassie G/Documents/390FinalProject-1/LeoJumpingData/Jumping1.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/LeoJumpingData/Jumping2.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/LeoJumpingData/Jumping3.csv'
+merge1 = ['C:/Users/Cassie G/Documents/390FinalProject-1/jumping_cg.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_hc.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_lp.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_cg.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_hc.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_lp.csv'
          ]
 
 data_frame1 = pd.DataFrame()
@@ -11,7 +14,9 @@ for csv1 in merge1:
     data1 = pd.read_csv(csv1)
     data_frame1 = pd.concat([data_frame1, data1], axis=0)
 
-data_frame1.to_csv('jumping_lp.csv', index=False)
+data_frame1.to_csv('datasetOverall.csv', index=False)
+
+'''
 
 merge2 = ['C:/Users/Cassie G/Documents/390FinalProject-1/LeoWalkingData/Walking1.csv',
          'C:/Users/Cassie G/Documents/390FinalProject-1/LeoWalkingData/Walking2.csv',
@@ -25,3 +30,4 @@ for csv2 in merge2:
     data_frame2 = pd.concat([data_frame2, data2], axis=0)
 
 data_frame2.to_csv('walking_lp.csv', index=False)
+'''
