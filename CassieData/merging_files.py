6b40ac21c58823a631e9d3f4d6ae1a5c2fe4343a
cassieg0 <c.greidanus@queensu.ca>
2023-04-03 23:21:28 -0400

    file names

--- CassieData/merging_files.py
+++ CassieData/merging_files.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-merge = ['Jumping_phone_pocket_cg.csv', 'file2.csv']
-df = pd.DataFrame()
-for file in merge:
-    data = pd.read_csv(file)
-    df = pd.concat([df, data], axis=0)
-df.to_csv('jumping_cg.csv', index=False)
+merge = ["JumpingPhonePocketcg.csv", "jumpingPhoneHandcg.csv", "jumpingcg.csv"]
+
+data_frame = pd.DataFrame()
+
+for csv in merge:
+    data = pd.read_csv(csv)
+    data_frame = pd.concat([data_frame, data], axis=0)
+
+data_frame.to_csv('jumping_cg.csv', index=False)
