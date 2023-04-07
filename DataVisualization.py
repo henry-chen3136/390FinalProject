6e909f24757ba84c02c2fd06d3e8f89f1b7f7b33
cassieg0 <c.greidanus@queensu.ca>
2023-04-07 14:33:36 -0400

    edits

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,1 +1,27 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing data
+jump = pd.read_csv('C:/Users/Cassie G/Documents/390FinalProject-1/allJump.csv')
+walk = pd.read_csv('C:/Users/Cassie G/Documents/390FinalProject-1/allWalk.csv')
+both = pd.read_csv('C:/Users/Cassie G/Documents/390FinalProject-1/datasetOverall.csv')
+
+#renaming the columns in the dataset so it is easier to call 
+jump.rename(columns={"Time (s)" : "time", "Linear Acceleration x (m/s^2)" : "x", 
+                     "Linear Acceleration y (m/s^2)" : "y", "Linear Acceleration z (m/s^2)" : "z", 
+                     "Absolute acceleration (m/s^2)" : "abs"})
+
+walk.rename(columns={"Time (s)" : "time", "Linear Acceleration x (m/s^2)" : "x", 
+                     "Linear Acceleration y (m/s^2)" : "y", "Linear Acceleration z (m/s^2)" : "z", 
+                     "Absolute acceleration (m/s^2)" : "abs"})
+
+both.rename(columns={"Time (s)" : "time", "Linear Acceleration x (m/s^2)" : "x", 
+                     "Linear Acceleration y (m/s^2)" : "y", "Linear Acceleration z (m/s^2)" : "z", 
+                     "Absolute acceleration (m/s^2)" : "abs"})
+
+'''
+#plotting x and z variables within each dataset to compare
+jumpXZ = plt.scatter(jump.loc[:,["x"]], jump.loc[:,["z"]])
+walkXZ = plt.scatter(walk.loc[:,["x"]], walk.loc[:,["z"]])
+bothXZ = plt.scatter(both.loc[:,["x"]], both.loc[:,["z"]])
+'''
