469f4dc805dabd43d975ff0db3dd54f1a3210325
cassieg0 <c.greidanus@queensu.ca>
2023-04-07 08:35:59 -0400

    newDataSet

--- merge_code.py
+++ merge_code.py
@@ -2,10 +2,7 @@ import pandas as pd
 
 merge1 = ['C:/Users/Cassie G/Documents/390FinalProject-1/jumping_cg.csv',
          'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_hc.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_lp.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_cg.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_hc.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_lp.csv'
+         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_lp.csv'
          ]
 
 data_frame1 = pd.DataFrame()
@@ -14,13 +11,11 @@ for csv1 in merge1:
     data1 = pd.read_csv(csv1)
     data_frame1 = pd.concat([data_frame1, data1], axis=0)
 
-data_frame1.to_csv('datasetOverall.csv', index=False)
+data_frame1.to_csv('allJump.csv', index=False)
 
-'''
-
-merge2 = ['C:/Users/Cassie G/Documents/390FinalProject-1/LeoWalkingData/Walking1.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/LeoWalkingData/Walking2.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/LeoWalkingData/Walking3.csv'
+merge2 = ['C:/Users/Cassie G/Documents/390FinalProject-1/walking_cg.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_hc.csv',
+         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_lp.csv'
          ]
 
 data_frame2 = pd.DataFrame()
@@ -29,5 +24,4 @@ for csv2 in merge2:
     data2 = pd.read_csv(csv2)
     data_frame2 = pd.concat([data_frame2, data2], axis=0)
 
-data_frame2.to_csv('walking_lp.csv', index=False)
-'''
+data_frame2.to_csv('allWalk.csv', index=False)
