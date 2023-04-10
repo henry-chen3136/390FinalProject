8a40dd7a283d49caf59c315afac4572068cada93
Excaleobur <leopalerma@gmail.com>
2023-04-10 16:15:24 -0400

    changes

--- mergetoOverall.py
+++ mergetoOverall.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
-merge1 = ['C:/Users/Cassie G/Documents/390FinalProject-1/jumping_cg.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_hc.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_lp.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_cg.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_hc.csv',
-         'C:/Users/Cassie G/Documents/390FinalProject-1/walking_lp.csv'
-         ]
+# merge1 = ['C:/Users/Cassie G/Documents/390FinalProject-1/jumping_cg.csv',
+#          'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_hc.csv',
+#          'C:/Users/Cassie G/Documents/390FinalProject-1/jumping_lp.csv',
+#          'C:/Users/Cassie G/Documents/390FinalProject-1/walking_cg.csv',
+#          'C:/Users/Cassie G/Documents/390FinalProject-1/walking_hc.csv',
+#          'C:/Users/Cassie G/Documents/390FinalProject-1/walking_lp.csv'
+#          ]
+
+merge1 = ['allJump.csv','allWalk.csv']
 
 data_frame1 = pd.DataFrame()
 
